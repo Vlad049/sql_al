e6529ac759895c087aa11fb542e8d5eaffa3f78a
Vlad049 <vladbuksa7@gmail.com>
2024-11-23 21:48:28 +0200

    first commit

---
+++ app/db/models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.db.base import Base
+
+
+class Pizza(Base):
+    __tablename__ = "pizzas"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+    ingredients: Mapped[str] = mapped_column(String())
+    price: Mapped[float] = mapped_column()
+
+
+class Position(Base):
+    __tablename__ = "positions"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+
+
+class Employee(Base):
+    __tablename__ = "employees"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100))
+    position_id: Mapped[int] = mapped_column(ForeignKey(Position.id))
+    position: Mapped[position] = relationship()
---
+++ main.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, redirect
+
+from app.db.base import create_db, Session
+from app.db.models import Pizza, Employee, Position
+
+
+app = Flask(__name__, template_folder="app/templates")
+
+
+@app.get("/")
+def index():
+    with Session() as session:
+        employees = session.query(Employee).all()
+        return render_template("index.html", employees=employees)
+    
+
+@app.route("/add_employee/", methods=["GET", "POST"])
+def add_employee():
+    with Session() as session:
+        positions = session.query(Position).all()
+
+        if request.method == "POST":
+            name = request.form.get("name")
+            position_id = request.form.get("position_id")
+            employee = employee(name=name, position_id=position_id)
+            session.add(employee)
+            session.commit()
+            return redirect("/")
+
+        return render_template("add_employee.html", positions=positions)
+    
+
+@app.route("/add_position/", methods=["GET", "POST"])
+def add_position():
+    if request.method == "POST":
+        with Session() as session:
+            name = request.form.get("name")
+            position = position(name=name)
+            session.add(position)
+            session.commit()
+            return redirect("/")
+
+    return render_template("add_position.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=90)
